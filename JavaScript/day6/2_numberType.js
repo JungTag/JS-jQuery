dee626990c1cfcd5bcbec9d4374be753e7acb210
JungTag <dbsdudxor121@naver.com>
2020-08-06 02:04:26 +0900

    6일차 과제 업로드

---
+++ JavaScript/day6/2_numberType.js
@@ -0,0 +1,16 @@
+var width = 10;
+var height = 20;
+
+// 둘레의 길이 구하기
+var perimeter;
+perimeter = (width + height)* 2
+// 코드를 작성하세요.
+
+// 넓이 구하기
+var area;
+area = width * height
+// 코드를 작성하세요.
+
+// 테스트 코드
+console.log("둘레의 길이: " + perimeter);
+console.log("넓이: " + area);
